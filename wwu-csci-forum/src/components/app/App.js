601ec5ceb6ec2c702b945302518d0f18eb04ac82
gcran <craneg@wwu.edu>
2023-02-04 17:53:48 -0800

    added material ui to project, loaded a basic card example from website docs

--- wwu-csci-forum/src/components/app/App.js
+++ wwu-csci-forum/src/components/app/App.js
@@ -3,13 +3,15 @@ import Post from '../post/post';
 import Login from '../login/login';
 import Signup from '../login/signup';
 import Button from '../button/button';
+import MainThread from '../thread/mainThread';
 import './App.css';
 
 function App() {
     return (
-        <>
+      <>
+        <MainThread/>
         <Post/>
-        </>
+      </>
     );
 }
 
--- wwu-csci-forum/src/components/button/button.js
+++ wwu-csci-forum/src/components/button/button.js
@@ -6,11 +6,9 @@ import './button.css'
 
 function Button(props) {
   return (
-    
-        <button className = "Button" onClick={props.onClick}>
-        {props.children}
-        </button>
-   
+    <button className = "Button" onClick={props.onClick}>
+      {props.children}
+    </button>
   )
 };
 
