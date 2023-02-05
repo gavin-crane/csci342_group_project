d135e3fe036de7643b6ec88766c3b46f5bf05d30
gcran <craneg@wwu.edu>
2023-02-04 23:06:11 -0800

    removed post testing dummy data

--- wwu-csci-forum/src/components/app/App.js
+++ wwu-csci-forum/src/components/app/App.js
@@ -6,22 +6,6 @@ import Button from '../button/button';
 import MainThread from '../thread/mainThread';
 import './App.css';
 
-let postDetails = {
-  title: 'My first post',
-  author: 'John Doe',
-  content: 'This is my text body sda;kjfh asdkfj asdlkf gasdlkfjg hasdfkjalsd flkjadsfhasdjkfhasd hflaskjdh fhlasjdkf',
-  replies: {
-    author: 'author of reply',
-    content: 'content of reply',
-    replies: {
-      author: 'author of reply of reply',
-      content: 'content of reply of reply',
-    },
-    author: 'author of reply 2',
-    content: 'content of reply 2',
-    replies: {},
-  } 
-}
 
 let postDetails2 = {
   title: 'My first post',
